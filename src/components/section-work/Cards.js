8d953155a92f7f8bf05f4cc1a579cbf26ac7e60e
Dmitry Milevsky <bdodinka1992@gmail.com>
2022-10-08 13:58:45 +0300

    feat:refactor Cards work

--- src/components/section-work/Cards.js
+++ src/components/section-work/Cards.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import style from './cards.module.css'
+import {myProject} from "../../bll/bll";
 
 const Cards = () => {
     return (
         <div className={style.cards}>
-            <div className={style.card}>
-                <div className={style.ava_card}>
-                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                    <a href='#' className={style.a_item}>React</a>
+
+            {myProject.map(i=>
+                <div className={style.card}>
+                    <div className={style.ava_card} style={{
+                        background: `url(${i.img})`
+                    }}>
+                    </div>
+                    <a href='#' className={style.a_item}>{i.btn}</a>
+                    <div className={style.box_description}>
+                        <h4 className={style.title}>{i.title}</h4>
+                        <p className={style.description}>{i.description}</p>
+                    </div>
                 </div>
-                <div className={style.box_description}>
-                    <h4 className={style.title}>Название Проекта</h4>
-                    <p className={style.description}>краткое описание</p>
-                </div>
-            </div>
-            <div className={style.card}>
-                <div className= {style.ava_card}>
-                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                    <a href='#' className={style.a_item}>JS</a>
-                </div>
-                <div className={style.box_description}>
-                    <h4 className={style.title}>Название Проекта</h4>
-                    <p className={style.description}>краткое описание</p>
-                </div>
-            </div>
+
+            )}
+
             </div>
 
     );
